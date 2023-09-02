72515db9123ea133cc167e07e5d74fdbb9993637
gnoblet <gnoblet@zaclys.net>
2023-09-02 19:18:22 +0200

    hwise: Add default column names

--- R/hwise_brief.R
+++ R/hwise_brief.R
@@ -1,18 +1,23 @@
 #' @title HWISE scores - Brief Household Water Insecurity Experiences (HWISE-4) Scale
 #'
 #' @param df A data frame
-#' @param hwise_worry Component column: Worry
-#' @param hwise_plans Component column: Plans
-#' @param hwise_hands Component column: Hands
-#' @param hwise_drink Component column: Drink
-#' @param level_codes Character vector of responses codes, including first in the following order: "Never (0 times)", "Rarely (1–2 times)", "Sometimes (3–10 times)", "Often (11-20 times)", "Always (more than 20 times)", "Do not know", "Prefer not to answer", "Not applicable", e.g. c("never", "rarely", "sometimes", "often", "always", "do_not_now", "prefer_not_to_answer", "not_applicable")
+#' @param hwise_worry Component column: Worry.
+#' @param hwise_plans Component column: Plans.
+#' @param hwise_hands Component column: Hands.
+#' @param hwise_drink Component column: Drink.
+#' @param level_codes Character vector of responses codes, including first in the following order: "Never (0 times)", "Rarely (1–2 times)", "Sometimes (3–10 times)", "Often (11-20 times)", "Always (more than 20 times)", "Do not know", "Prefer not to answer", "Not applicable", e.g. c("never", "rarely", "sometimes", "often", "always", "do_not_know", "prefer_not_to_answer", "not_applicable").
 #'
 #' @return Eight new columns: each component score (hwise_brief_score_*), the overall score (hwise_brief_score), dummy for water insecurity (hwise_brief_insecure), the broader categories (hwise_brief_cat), the detailed categories (hwise_cat_details).
 #'
 #' @importFrom rlang `:=`
 #'
 #' @export
-hwise_brief <- function(df, hwise_worry, hwise_plans, hwise_hands, hwise_drink, level_codes){
+hwise_brief <- function(df,
+                        hwise_worry = "hwise_worry",
+                        hwise_plans = "hwise_plans",
+                        hwise_hands = "hwise_hands",
+                        hwise_drink = "hwise_drink",
+                        level_codes =  c("never", "rarely", "sometimes", "often", "always", "do_not_know", "prefer_not_to_answer", "not_applicable")){
 
   #------ Enquos
   hwise_comp_cols <- rlang::enquos(hwise_worry, hwise_plans, hwise_hands, hwise_drink)
