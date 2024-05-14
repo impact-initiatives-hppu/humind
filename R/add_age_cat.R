ec29f063f31291841b4e8cdd2f8d3490365a3e63
gnoblet <gnoblet@zaclys.net>
2024-05-14 15:18:08 +0200

    Add the recoding of ages and num vars

---
+++ R/add_age_cat.R
@@ -0,0 +1,70 @@
+#' Add categories of age
+#'
+#' [add_age_cat()] adds the age categories and [add_age_18_cat()] adds the age categories and a dummy variable for below and above 18.
+#'
+#' @param df A dataframe.
+#' @param age_col The column name to recategorize.
+#' @param breaks A vector of cut points.
+#' @param labels A vector of labels. If NULL, the labels will be the breaks.
+#' @param int_undefined A vector of values undefined (such as -999, 999) to replace by char_undefined.
+#' @param char_undefined A character to replace int_undefined values, often values corresponding to Don't know of Pre.
+#' @param new_colname The name of the new column. If NULL, it adds "_cat" to the age_col (or "_18_cat for `add_age_18_cat()`).
+#'
+#' @section Details:
+#'
+#' * [add_age_18_cat()] adds two columns to the dataframe: one with the categories (minor, major) and one with a dummy variable for below and above 18.
+#'
+#' @export
+add_age_cat <- function(df, age_col, breaks = seq(14, 100, by  = 5), labels = NULL, int_undefined = c(-999, 999), char_undefined = "undefined", new_colname = NULL) {
+
+  # Use categorize_num function
+  df <- add_num_cat(
+    df = df,
+    num_col = age_col,
+    breaks = breaks,
+    labels = labels,
+    int_undefined = int_undefined,
+    char_undefined = char_undefined,
+    new_colname = new_colname,
+    include_lowest = TRUE)
+
+  return(df)
+}
+
+#' @rdname add_age_cat
+#'
+#' @export
+add_age_18_cat <- function(df, age_col, int_undefined = c(-999, 999), char_undefined = "undefined", new_colname = NULL) {
+
+  # If new_colname is not provided, create one
+  if (is.null(new_colname)) new_colname <- paste0(age_col, "_18_cat")
+
+  # Paste "_d" for the new dummy column
+  new_colname_d <- paste0(new_colname, "_d")
+
+  df <- add_num_cat(
+    df = df,
+    num_col = age_col,
+    breaks = 18,
+    labels = c("below_18", "above_18"),
+    int_undefined = int_undefined,
+    char_undefined = char_undefined,
+    new_colname = new_colname,
+    include_lowest = TRUE
+  )
+
+  df <- dplyr::mutate(
+    df,
+    # Dummy 1 or 0 column
+    "{new_colname_d}"  := dplyr::case_when(
+      !!rlang::sym(new_colname) == char_undefined ~ NA_integer_,
+      # Replace values below 18 by 1
+      !!rlang::sym(new_colname) == "below_18" ~ 1,
+      # Replace values above 18 by 0
+      !!rlang::sym(new_colname) == "above_18" ~ 0,
+      .default = NA_integer_
+    )
+  )
+
+  return(df)
+}
---
+++ R/add_num_cat.R
@@ -0,0 +1,57 @@
+#' Add categories for a numeric variable
+#'
+#' @param df A dataframe.
+#' @param num_col The column name to recategorize.
+#' @param breaks A vector of cut points.
+#' @param labels A vector of labels.
+#' @param int_undefined A vector of values to replace by char_dontknow.
+#' @param char_undefined A character to replace int_dontknow values.
+#' @param new_colname The name of the new column.
+#' @param include_lowest Logical. Include the lowest value of the interval.
+#'
+#' @return A dataframe with a new column.
+#'
+#' @export
+add_num_cat <- function(df, num_col, breaks, labels = NULL, int_undefined = c(-999, 999), char_undefined = "Unkown", new_colname = NULL, include_lowest = TRUE) {
+
+  #------ Checks
+
+  # Check if num_col is in df
+  if_not_in_stop(df, num_col, "num_col")
+
+  # Check if num_col is numeric
+  are_cols_numeric(df, num_col)
+
+  # Check if int_dontknow is numeric
+  if (!all(sapply(int_undefined, is.numeric))) rlang::abort("int_dontknow must only contain numeric values")
+
+  # Check if char_dontknow is of length 1
+  if (length(char_undefined) != 1) rlang::abort("char_dontknow must be of length 1")
+
+  # Check if char_dontknow is character
+  if (!is.character(char_undefined)) rlang::abort("char_dontknow must be a character")
+
+  # Check if breaks have at least two values
+  if (length(breaks) < 2) rlang::abort("breaks must have at least two values")
+
+  # Check if labels is of length of breaks + 1
+  if (!is.null(labels) && length(labels) != length(breaks) + 1) {
+    rlang::warn("labels must be of length of breaks + 1. Reverting to labels = NULL")
+    labels <- NULL
+  }
+
+  # If new_colname is not provided, create one
+  if (is.null(new_colname)) new_colname <- paste0(num_col, "_cat")
+
+  #Create
+  df <- dplyr::mutate(df, "{new_colname}" := dplyr::case_when(
+    # Replace value in int_dontkow by char_dontknow
+    !!rlang::sym(num_col) %in% int_undefined ~ char_undefined,
+    # Replace values below 0 by NA
+    !!rlang::sym(num_col) < 0 ~ NA_character_,
+    # Create categories using function cut and breaks
+    .default = cut(!!rlang::sym(num_col), breaks = breaks, labels = labels, include.lowest = include_lowest)
+  ))
+
+  return(df)
+}
