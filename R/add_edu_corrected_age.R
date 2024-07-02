da76bbb4029618dd5fe201dfbcdd38dee9d75244
gnoblet <gnoblet@zaclys.net>
2024-07-02 13:24:16 +0200

    Fixed variables names and coding style

--- R/add_edu_corrected_age.R
+++ R/add_edu_corrected_age.R
@@ -1,4 +1,4 @@
-#' @title Add column edu_ind_corrected_age with the revised and corrected individual age according to the start of the data collection wrt the start f the school-year
+#' Add a corrected schooling age  edu_ind_corrected_age with the revised and corrected individual age according to the start of the data collection wrt the start f the school-year
 #' Add a column edu_is_school_child
 
 #' @param roster A data frame of individual-level data.
@@ -10,34 +10,47 @@
 
 
 
-add_edu_corrected_age <- function(roster, household_data, start_school_year = 9, age_col = 'ind_age') {
-  # Convert the 'start' column to month and create a new data frame
-  df <- household_data %>%
-    mutate(month = as.integer(format(as.Date(start), "%m")))
+add_edu_ind_age_corrected <- function(loop, main, id_col = "uuid", survey_start_date = "start", school_year_start_month = 9, ind_age = "ind_age") {
+  
+  # Convert the survey start date column to month
+  main <- dplyr::mutate(main, month = as.integer(format(as.Date(!!rlang::sym(survey_start_date)), "%m")))
+  main <- dplyr::select(main, dplyr::all_of(id_col, "month"))
 
-  # Join df to roster
-  roster <- roster %>%
-    left_join(select(df, uuid, month), by = "uuid")
+  # Join main to loop
+  loop <- dplyr::left_join(loop, main, by = id_col)
 
   # Calculate the adjusted start month number
-  adjusted_start_month_num <- ifelse(start_school_year > 6, start_school_year - 12, start_school_year)
+  school_year_start_month_adj <- ifelse(school_year_start_month > 6, school_year_start_month - 12, school_year_start_month)
 
-  roster[[age_col]] <- as.numeric(roster[[age_col]])
+  # we should add a test that it's numeric instead
+  # roster[[age_col]] <- as.numeric(roster[[age_col]])
 
   # Apply the age correction based on the month difference, handling NA in month or age
-  roster <- roster %>%
-    mutate(edu_ind_corrected_age = ifelse(!is.na(month) & !is.na(!!sym(age_col)) & (month - adjusted_start_month_num) > 6,
-                                          !!sym(age_col) - 1,
-                                          !!sym(age_col)))
-  roster <- roster %>%
-    mutate(edu_ind_corrected_age = ifelse(edu_ind_corrected_age < 5 | edu_ind_corrected_age > 17, NA_real_, edu_ind_corrected_age))
+  loop <- dplyr::mutate(loop, edu_ind_age_corrected = dplyr::case_when(
+    !is.na(!!rlang::sym("month")) & !is.na(!!sym(ind_age)) & (!!rlang::sym("month") - school_year_start_month_adj) > 6 ~ !!rlang::sym(ind_age) - 1,
+    .default = !!rlang::sym(ind_age)
+    )
+  )
 
+  #Final classification --- NAing with below 5 or under 17
+  loop <- dplyr::mutate(
+    loop, 
+    edu_ind_age_corrected = dplyr::case_when(
+      !!rlang::sym("edu_ind_age_corrected") < 5 | !!rlang::sym("edu_ind_age_corrected") > 17 ~ NA_real_,
+      .default = !!rlang::sym("edu_ind_age_corrected")
+    )
+  )
 
-  roster <- roster %>%
-    mutate(edu_is_school_child = if_else(is.na(edu_ind_corrected_age) | (edu_ind_corrected_age < 5 | edu_ind_corrected_age > 17), 0, 1))
-
-  # Optionally, remove the month column if it's no longer needeS
-  roster <- roster %>% select(-month)
-  return (roster)
+  # Add a dummy variable, 1 if the individual is a school child, 0 otherwise
+  loop <- dplyr::mutate(
+    loop, 
+    edu_ind_schooling_age_d = dplyr::case_when(
+      is.na(!!rlang::sym("edu_ind_age_corrected")) ~ 0
+      !!rlang::sym("edu_ind_age_corrected") < 5 | !!rlang::sym("edu_ind_age_corrected") > 17 ~ 0,
+      .default = 1
+    )
+  )
+  
+  return (loop)
 }
 
