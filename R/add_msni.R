9f80ea65591bd97a0cc6165f581c9aaf60180eae
gnoblet <gnoblet@zaclys.net>
2024-07-12 08:55:01 +0200

    Add metrics 3 and 4

--- R/add_msni.R
+++ R/add_msni.R
@@ -1,6 +1,6 @@
 #' Add MSNI - add score and dummy for in need
 #'
-#' The output is a data frame with three new columns: `msni_score`, "msni_in_need", and "msni_in_acute_need".`
+#' The output is a data frame with 5 new columns. First the MSNI-related variables: `msni_score`, `msni_in_need`, and `msni_in_acute_need`. The two latter are used for metrics 1 and 2. Second, the number of sectoral needs `sector_in_need_n` and the sectoral needs profile `sector_needs_profile`. These two are used for metric 3 and 4.
 #'
 #' @param df A data frame.
 #' @param comp_foodsec_score Column name for the food security composite score.
@@ -18,30 +18,52 @@ add_msni <- function(
   comp_wash_score = "comp_wash_score",
   comp_prot_score = "comp_prot_score",
   comp_health_score = "comp_health_score",
-  comp_edu_score = "comp_edu_score"){
+  comp_edu_score = "comp_edu_score",
+  comp_foodsec_in_need = "comp_foodsec_in_need",
+  comp_snfi_in_need = "comp_snfi_in_need",
+  comp_wash_in_need = "comp_wash_in_need",
+  comp_prot_in_need = "comp_prot_in_need",
+  comp_health_in_need = "comp_health_in_need",
+  comp_edu_in_need = "comp_edu_in_need"
+  ){
 
   #------ Checks
 
   # All vars
   comp_scores <- c(comp_foodsec_score, comp_snfi_score, comp_wash_score, comp_prot_score, comp_health_score, comp_edu_score)
+  comp_in_need <- c(comp_foodsec_in_need, comp_snfi_in_need, comp_wash_in_need, comp_prot_in_need, comp_health_in_need, comp_edu_in_need)
+  comp_names <- c("Food security", "SNFI", "WASH", "Protection", "Health", "Education")
 
-  # Check that variables are in df, if they are not, throw a warning for the ones missing or stop if all are missing
-  comp_scores_lgl <- comp_scores %in% df
+  # Check that score variables are in df, if they are not, throw a warning for the ones missing or stop if all are missing
+  comp_scores_lgl <- comp_scores %in% colnames(df)
   comp_scores_nin <- comp_scores[!comp_scores_lgl]
   comp_scores <- comp_scores[comp_scores_lgl]
   # all missing
-  if (all(!comp_scores_lgl)) {
+  if (!all(comp_scores_lgl)) {
     rlang::abort(paste("There are none of the sectoral composites variables. Are you sure you specified the names correctly or you have run the necessary functions to add them?"))
   }
   # Some missing
   if (any(!comp_scores_lgl)) {
-    rlang::warn(paste("The following variables are not in the data frame and the calculation will be run without them:", paste(comp_scores_nin, collapse = ", ")))
+    rlang::warn(paste("The following variables are not in the data frame and the MSNI calculation will be run without them:", paste(comp_scores_nin, collapse = ", ")))
   }
-
-  # If length
 
   # Check that all values are in set 1:5
   are_values_in_set(df, comp_scores, 1:5)
+
+
+  # Check that the dummy variables for in need are in df
+  comp_in_need_lgl <- comp_in_need %in% colnames(df)
+  comp_in_need_nin <- comp_in_need[!comp_in_need_lgl]
+  comp_in_need <- comp_in_need[comp_in_need_lgl]
+  comp_names <- comp_names[comp_in_need_lgl]
+  # all missing
+  if (!all(comp_in_need_lgl)) {
+    rlang::abort(paste("There are none of the sectoral composites 'in need' variables. Are you sure you specified the names correctly or you have run the necessary functions to add them?"))
+  }
+  # Some missing
+  if (any(!comp_in_need_lgl)) {
+    rlang::warn(paste("The following variables are not in the data frame and the number of sectoral needs and the needs profile calculation will be run without them:", paste(comp_in_need_nin, collapse = ", ")))
+  }
 
   #------ Take the maximum
 
@@ -68,6 +90,30 @@ add_msni <- function(
     score = "msni_score",
     new_colname = "msni_in_acute_need"
   )
+
+  #------ Add number of sectoral needs
+
+  df <- sum_vars(
+    df,
+    comp_in_need,
+    "sector_in_need_n",
+    na_rm = TRUE,
+    imputation = "none")
+
+  #------ Add needs profiles
+
+  df_comp_in_need <-  dplyr::select(df, dplyr::all_of(comp_in_need))
+
+  df$sector_needs_profile <- purrr::pmap_chr(
+    df_comp_in_need,
+    function(...){
+      values <- c(...)
+      labels <- comp_names[values == 1 & !is.na(values)]
+      paste(labels, collapse = " - ")
+    })
+
+
+  #------ Return
 
   return(df)
 
