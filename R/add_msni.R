4e45a29aa91225d85914616a5d27411b47bc1c7c
gnoblet <gnoblet@zaclys.net>
2024-07-12 07:55:37 +0200

    Add warning and allow for missing sectzors

--- R/add_msni.R
+++ R/add_msni.R
@@ -1,3 +1,16 @@
+#' Add MSNI - add score and dummy for in need
+#'
+#' The output is a data frame with three new columns: `msni_score`, "msni_in_need", and "msni_in_acute_need".`
+#'
+#' @param df A data frame.
+#' @param comp_foodsec_score Column name for the food security composite score.
+#' @param comp_snfi_score Column name for the SNFI composite score.
+#' @param comp_wash_score Column name for the WASH composite score.
+#' @param comp_prot_score Column name for the protection composite score.
+#' @param comp_health_score Column name for the health composite score.
+#' @param comp_edu_score Column name for the education composite score.
+#'
+#' @export
 add_msni <- function(
   df,
   comp_foodsec_score = "comp_foodsec_score",
@@ -12,20 +25,26 @@ add_msni <- function(
   # All vars
   comp_scores <- c(comp_foodsec_score, comp_snfi_score, comp_wash_score, comp_prot_score, comp_health_score, comp_edu_score)
 
-  # Check that variables are in df
-  if_not_in_stop(df, comp_scores, "df")
+  # Check that variables are in df, if they are not, throw a warning for the ones missing
+  comp_in_df <- comp_scores %in% df
+  if (any(!comp_in_df)) {
+    rlang::warn(paste("The following variables are not in the data frame and the calculation will be run without them:", paste(comp_scores[!comp_in_df], collapse = ", ")))
+  }
+
+  # Keep only existing ones
+  comp_scores <- comp_scores[comp_in_df]
 
   # Check that all values are in set 1:5
   are_values_in_set(df, comp_scores, 1:5)
 
   #------ Take the maximum
 
-  df <- impactR.utils::row_optimum(
-    df = df,
-    !!!rlang::syms(comp_scores),
-    optimum = "max",
-    max_name = "msni_score",
-    na_rm = TRUE
+  df <- dplyr::mutate(
+    df,
+    msni_score = pmax(
+      !!!rlang::syms(comp_scores),
+      na.rm = TRUE
+    )
   )
 
   #------ Create dummy "in need"
