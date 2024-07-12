f876ed547a43470e2cbaaefc10dfa10eda5b970a
gnoblet <gnoblet@zaclys.net>
2024-07-12 08:28:57 +0200

    Stop if all are missing

--- R/add_msni.R
+++ R/add_msni.R
@@ -25,14 +25,23 @@ add_msni <- function(
   # All vars
   comp_scores <- c(comp_foodsec_score, comp_snfi_score, comp_wash_score, comp_prot_score, comp_health_score, comp_edu_score)
 
-  # Check that variables are in df, if they are not, throw a warning for the ones missing
-  comp_in_df <- comp_scores %in% df
-  if (any(!comp_in_df)) {
-    rlang::warn(paste("The following variables are not in the data frame and the calculation will be run without them:", paste(comp_scores[!comp_in_df], collapse = ", ")))
+  # Check that variables are in df, if they are not, throw a warning for the ones missing or stop if all are missing
+  comp_scores_lgl <- comp_scores %in% df
+  comp_scores_nin <- comp_scores[!comp_scores_lgl]
+  comp_scores <- comp_scores[comp_in_df]
+  # all missing
+  if (all(!comp_scores_lgl)) {
+    rlang::abort(paste("There are none of the sectoral composites variables. Are you sure you specified the names correctly or you have run the necessary functions to add them?"))
+  }
+  # Some missing
+  if (any(!comp_scores_lgl)) {
+    rlang::warn(paste("The following variables are not in the data frame and the calculation will be run without them:", paste(comp_scores_nin, collapse = ", ")))
   }
 
   # Keep only existing ones
   comp_scores <- comp_scores[comp_in_df]
+
+  # If length
 
   # Check that all values are in set 1:5
   are_values_in_set(df, comp_scores, 1:5)
