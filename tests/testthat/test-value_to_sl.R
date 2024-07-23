d30b0e74b066440eb57713cb3cdb30253d3da3d4
lanawwas <lanawwas@jouna.org>
2024-07-23 14:27:47 +0200

    resolved #321 #320 #319 #318 #317 #166 #165 #164 #163 #162 #161 #160 #159 #158 #157 #156 #155 #154 #153 #152 #151 #150 #149 #148 #147 #138 #137 #135 #134 #130 #129 #128 #127

--- tests/testthat/test-value_to_sl.R
+++ tests/testthat/test-value_to_sl.R
@@ -12,28 +12,28 @@ df <- tibble::tibble(
 test_that("value_to_sl adds sl_value correctly", {
   result <- value_to_sl(df, var = "var", undefined = c("a", "d"), sl_vars = c("sl_var1", "sl_var2"), sl_value = 10)
 
-  expect_equal(result$sl_var1, c(NA, 2, 10, NA))
-  expect_equal(result$sl_var2, c(NA, NA, 10, NA))
+  expect_equal(result$sl_var1, c(NA, 2, 10, 4))
+  expect_equal(result$sl_var2, c(NA, 10, 3, 4))
 })
 
 test_that("value_to_sl skips adding sl_value when var is undefined", {
   result <- value_to_sl(df, var = "var", undefined = c("a", "d"), sl_vars = c("sl_var1", "sl_var2"), sl_value = 10)
 
-  expect_equal(result$sl_var1, c(NA, 2, 10, NA)) # Check that values are correctly unchanged where var is "a" or "d"
-  expect_equal(result$sl_var2, c(NA, NA, 10, NA))
+  expect_equal(result$sl_var1, c(NA, 2, 10, 4))
+  expect_equal(result$sl_var2, c(NA, 10, 3, 4))
 })
 
 test_that("value_to_sl handles non-NA sl_vars correctly", {
   result <- value_to_sl(df, var = "var", undefined = NULL, sl_vars = c("sl_var1", "sl_var2"), sl_value = 10)
 
-  expect_equal(result$sl_var1, c(10, 2, 10, 10))
-  expect_equal(result$sl_var2, c(10, 10, 3, 10))
+  expect_equal(result$sl_var1, c(10, 2, 10, 4))
+  expect_equal(result$sl_var2, c(10, 10, 3, 4))
 })
 
 test_that("value_to_sl adds suffix to new variable names", {
   result <- value_to_sl(df, var = "var", undefined = NULL, sl_vars = c("sl_var1", "sl_var2"), sl_value = 10, suffix = "_new")
 
-  expect_equal(result$sl_var1_new, c(10, 2, 10, 10))
-  expect_equal(result$sl_var2_new, c(10, 10, 3, 10))
+  expect_equal(result$sl_var1_new, c(10, 2, 10, 4))
+  expect_equal(result$sl_var2_new, c(10, 10, 3, 4))
   expect_true(all(c("sl_var1_new", "sl_var2_new") %in% names(result)))
 })
