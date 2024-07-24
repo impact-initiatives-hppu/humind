d2d0e59dbf778cde86f7074177dd436e28c42055
gnoblet <gnoblet@zaclys.net>
2024-07-24 15:59:29 +0200

    Fix #98

--- tests/testthat/test-add_sanitation_facility_cat.R
+++ tests/testthat/test-add_sanitation_facility_cat.R
@@ -36,7 +36,11 @@ test_that("add_sharing_sanitation_facility_num_ind works correctly", {
   df_test <- add_sharing_sanitation_facility_cat(df_test)
   result <- add_sharing_sanitation_facility_num_ind(df_test)
   expected_sharing_n <- (df$wash_sanitation_facility_sharing_n - 1) * mean_hh_size + df$hh_size
+  # [2] and [4] Not shared so only hhsize
   expected_sharing_n[2] <- df$hh_size[2]
+  expected_sharing_n[5] <- df$hh_size[5]
+  # [3] Undefined so NA
+  expected_sharing_n[3] <- NA
   expect_true("wash_sharing_sanitation_facility_n_ind" %in% colnames(result))
   expect_equal(result$wash_sanitation_facility_sharing_n, expected_sharing_n)
   expect_equal(result$wash_sharing_sanitation_facility_n_ind, c("19_and_below", "19_and_below", "19_and_below", "20_to_49", "19_and_below"))
