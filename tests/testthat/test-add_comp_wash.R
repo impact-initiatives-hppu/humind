c1d865b002ad2eca8267d51cd600a135b4270a24
lanawwas <lanawwas@jouna.org>
2024-07-24 16:04:47 +0200

    update test-add_comp_wash to adapt function new changes

--- tests/testthat/test-add_comp_wash.R
+++ tests/testthat/test-add_comp_wash.R
@@ -6,11 +6,11 @@ library(dplyr)
 dummy_data <- data.frame(
   setting = c("camp", "urban", "rural", NA),
   wash_drinking_water_quantity = c("always", "often", "sometimes", "rarely"),
-  wash_drinking_water_jmp_cat = c("safely_managed", "basic", "limited", "unimproved"),
+  wash_drinking_water_quality_jmp_cat = c("safely_managed", "basic", "limited", "unimproved"),
   wash_sanitation_facility_jmp_cat = c("safely_managed", "basic", "limited", "open_defecation"),
   wash_sanitation_facility_cat = c("improved", "unimproved", "none", "undefined"),
   wash_sharing_sanitation_facility_n_ind = c("19_and_below", "20_to_49", "50_and_above", NA),
-  wash_sanitation_facility_jmp_cat = c("basic", "limited", "no_facility", "undefined")
+  wash_handwashing_facility_jmp_cat = c("basic", "limited", "no_facility", "undefined")
 )
 
 # 1. Test the function with default parameters
@@ -24,11 +24,11 @@ test_that("Function works with default parameters", {
 undefined_data <- data.frame(
   setting = rep("camp", 4),
   wash_drinking_water_quantity = rep("dnk", 4),
-  wash_drinking_water_jmp_cat = rep("undefined", 4),
+  wash_drinking_water_quality_jmp_cat = rep("undefined", 4),
   wash_sanitation_facility_jmp_cat = rep("undefined", 4),
   wash_sanitation_facility_cat = rep("undefined", 4),
   wash_sharing_sanitation_facility_n_ind = rep(NA, 4),
-  wash_sanitation_facility_jmp_cat = rep("undefined", 4)
+  wash_handwashing_facility_jmp_cat = rep("undefined", 4)
 )
 
 test_that("Function handles all undefined categories", {
@@ -41,11 +41,11 @@ test_that("Function handles all undefined categories", {
 na_data <- data.frame(
   setting = c("camp", NA, "rural", "urban"),
   wash_drinking_water_quantity = c(NA, "often", "sometimes", NA),
-  wash_drinking_water_jmp_cat = c("safely_managed", NA, "limited", "unimproved"),
+  wash_drinking_water_quality_jmp_cat = c("safely_managed", NA, "limited", "unimproved"),
   wash_sanitation_facility_jmp_cat = c(NA, "basic", "limited", "open_defecation"),
   wash_sanitation_facility_cat = c("improved", NA, "none", "undefined"),
   wash_sharing_sanitation_facility_n_ind = c("19_and_below", "20_to_49", NA, "50_and_above"),
-  wash_sanitation_facility_jmp_cat = c("basic", "limited", "no_facility", NA)
+  wash_handwashing_facility_jmp_cat = c("basic", "limited", "no_facility", NA)
 )
 
 test_that("Function handles NA values in data", {
@@ -79,12 +79,12 @@ duplicated_column_data <- dummy_data
 duplicated_column_data$wash_drinking_water_quantity <- duplicated_column_data$setting
 
 test_that("Function handles duplicated column names", {
-  expect_error(add_comp_wash(duplicated_column_data), calss = "error")
+  expect_error(add_comp_wash(duplicated_column_data), class = "error")
 })
 
 # Test with empty data frame
 empty_data <- data.frame()
 
 test_that("Function handles empty data frame", {
-   expect_error(add_comp_wash(empty_data), class = "error")
+  expect_error(add_comp_wash(empty_data), class = "error")
 })
