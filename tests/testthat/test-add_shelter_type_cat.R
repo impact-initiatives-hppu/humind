4638857420dc6e3d620bd55496c6bc19a9cbafe8
lanawwas <lanawwas@jouna.org>
2024-07-24 16:55:25 +0200

    update tests

--- tests/testthat/test-add_shelter_type_cat.R
+++ tests/testthat/test-add_shelter_type_cat.R
@@ -1,24 +1,9 @@
 library(testthat)
 library(dplyr)
-library(rlang)
-
-# Mock the helper functions if_not_in_stop and are_values_in_set
-if_not_in_stop <- function(df, col, df_name) {
-  if (!col %in% colnames(df)) {
-    stop(paste("The column", col, "is not present in", df_name), call. = FALSE)
-  }
-}
-
-are_values_in_set <- function(df, col, valid_values) {
-  invalid_values <- setdiff(unique(df[[col]]), unlist(valid_values))
-  if (length(invalid_values) > 0) {
-    stop(paste("The following values in", col, "are not valid:", paste(invalid_values, collapse = ", ")), call. = FALSE)
-  }
-}
 
 test_that("add_shelter_type_cat works with different shelter types", {
   df <- data.frame(
-    snfi_shelter_type = c("none", "collective_center", "pnta", "other", "house"),
+    snfi_shelter_type = c("none", "collective_center", "pnta", "other", "individual_shelter"),
     snfi_shelter_type_individual = c("house", "makeshift", "tent", "other", "unfinished_building")
   )
 
