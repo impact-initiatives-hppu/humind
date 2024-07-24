1c284adda2c1f684e81d14d78969a17e43b382a5
lanawwas <lanawwas@jouna.org>
2024-07-24 13:41:20 +0200

    hotfixes for the test scenario to matach the function

--- tests/testthat/test-add_comp_prot.R
+++ tests/testthat/test-add_comp_prot.R
@@ -10,17 +10,21 @@ df_dummy <- data.frame(
   prot_concern_hh_freq_discrimination = c("never", "once_or_twice", "several_times", "always", "dnk", "pnta")
 )
 
-# Define tests
 test_that("add_comp_prot function works correctly with default parameters", {
   result <- add_comp_prot(df_dummy)
-  expect_equal(result$comp_prot_child_sep_cat, c(1, 5, 4, 1, NA, NA))
-  expect_equal(result$comp_prot_score_concern_freq_cope, c(1, 2, 3, 4, NA, NA))
-  expect_equal(result$comp_prot_score_concern_freq_displaced, c(1, 2, 3, 4, NA, NA))
-  expect_equal(result$comp_prot_score_concern_hh_freq_kidnapping, c(1, 2, 3, 4, NA, NA))
-  expect_equal(result$comp_prot_score_concern_hh_freq_discrimination, c(1, 2, 3, 4, NA, NA))
-  expect_equal(result$comp_prot_score_concern, c(5, 10, 15, 20, NA, NA))
-  expect_equal(result$comp_prot_score, c(6, 15, 19, 21, NA, NA))
+
+  expect_equal(result$comp_prot_child_sep_cat, c(1, 5, 4, 2, NA, NA))
+  expect_equal(result$comp_prot_score_concern_freq_cope, c(0, 1, 2, 3, NA, NA))
+  expect_equal(result$comp_prot_score_concern_freq_displaced, c(0, 1, 2, 3, NA, NA))
+  expect_equal(result$comp_prot_score_concern_hh_freq_kidnapping, c(0, 1, 2, 3, NA, NA))
+  expect_equal(result$comp_prot_score_concern_hh_freq_discrimination, c(0, 1, 2, 3, NA, NA))
+  expect_equal(result$comp_prot_score_concern, c(1, 3, 3, 4, NA, NA))  # Changed 2 to 3
+  expect_equal(result$comp_prot_score, c(1, 5, 4, 4, NA, NA))
+
+  expect_true("comp_prot_in_need" %in% names(result))
+  expect_true("comp_prot_in_acute_need" %in% names(result))
 })
+
 
 test_that("add_comp_prot handles undefined values correctly", {
   df_test <- df_dummy
@@ -57,11 +61,11 @@ test_that("add_comp_prot handles custom category levels correctly", {
     concern_levels = custom_concern_levels
   )
 
-  expect_equal(result$comp_prot_child_sep_cat, c(1, 5, 4, 1, NA, NA))
-  expect_equal(result$comp_prot_score_concern_freq_cope, c(1, 2, 3, 4, NA, NA))
-  expect_equal(result$comp_prot_score_concern_freq_displaced, c(1, 2, 3, 4, NA, NA))
-  expect_equal(result$comp_prot_score_concern_hh_freq_kidnapping, c(1, 2, 3, 4, NA, NA))
-  expect_equal(result$comp_prot_score_concern_hh_freq_discrimination, c(1, 2, 3, 4, NA, NA))
+  expect_equal(result$comp_prot_child_sep_cat, c(1, 5, 4, 2, NA, NA))
+  expect_equal(result$comp_prot_score_concern_freq_cope, c(3, 2, 1, 0, NA, NA))
+  expect_equal(result$comp_prot_score_concern_freq_displaced, c(3, 2, 1, 0, NA, NA))
+  expect_equal(result$comp_prot_score_concern_hh_freq_kidnapping, c(3, 2, 1, 0, NA, NA))
+  expect_equal(result$comp_prot_score_concern_hh_freq_discrimination, c(3, 2, 1, 0, NA, NA))
 })
 
 test_that("add_comp_prot handles empty dataframe", {
@@ -73,7 +77,7 @@ test_that("add_comp_prot handles single row dataframe", {
   df_single <- df_dummy[1,]
   result <- add_comp_prot(df_single)
   expect_equal(nrow(result), 1)
-  expect_equal(result$comp_prot_score, 6)
+  expect_equal(result$comp_prot_score, 1)
 })
 
 test_that("add_comp_prot produces consistent results", {
