30458f9bdb599fc8f1b0fe455b470a4a05d7a0b2
sunheemoraes <sunheemoraes@gmail.com>
2025-06-10 17:31:51 +0200

    update for issue581

--- tests/testthat/test-add_comp_prot.R
+++ tests/testthat/test-add_comp_prot.R
@@ -3,7 +3,6 @@ library(dplyr)
 
 # Create dummy data
 df_dummy <- data.frame(
-  prot_child_sep_cat = c("none", "at_least_one_very_severe", "at_least_one_severe", "at_least_one_non_severe", "undefined", NA),
   prot_concern_freq_cope = c("never", "once_or_twice", "several_times", "always", "dnk", "pnta"),
   prot_concern_freq_displaced = c("never", "once_or_twice", "several_times", "always", "dnk", "pnta"),
   prot_concern_hh_freq_kidnapping = c("never", "once_or_twice", "several_times", "always", "dnk", "pnta"),
@@ -13,37 +12,15 @@ df_dummy <- data.frame(
 test_that("add_comp_prot function works correctly with default parameters", {
   result <- add_comp_prot(df_dummy)
 
-  expect_equal(result$comp_prot_child_sep_cat, c(1, 5, 4, 2, NA, NA))
   expect_equal(result$comp_prot_score_concern_freq_cope, c(0, 1, 2, 3, NA, NA))
   expect_equal(result$comp_prot_score_concern_freq_displaced, c(0, 1, 2, 3, NA, NA))
   expect_equal(result$comp_prot_score_concern_hh_freq_kidnapping, c(0, 1, 2, 3, NA, NA))
   expect_equal(result$comp_prot_score_concern_hh_freq_discrimination, c(0, 1, 2, 3, NA, NA))
-  expect_equal(result$comp_prot_score_concern, c(1, 3, 3, 4, NA, NA))  # Changed 2 to 3
-  expect_equal(result$comp_prot_score, c(1, 5, 4, 4, NA, NA))
+  expect_equal(result$comp_prot_score_concern, c(1, 1, 2, 3, NA, NA))  # Changed 2 to 3
+  expect_equal(result$comp_prot_score, c(1, 1, 2, 3, NA, NA))
 
   expect_true("comp_prot_in_need" %in% names(result))
   expect_true("comp_prot_in_acute_need" %in% names(result))
-})
-
-
-test_that("add_comp_prot handles undefined values correctly", {
-  df_test <- df_dummy
-  df_test$prot_child_sep_cat <- "undefined"
-  result <- add_comp_prot(df_test)
-  expect_true(all(is.na(result$comp_prot_child_sep_cat)))
-})
-
-test_that("add_comp_prot handles NA values correctly", {
-  df_test <- df_dummy
-  df_test$prot_child_sep_cat[1] <- NA
-  result <- add_comp_prot(df_test)
-  expect_true(is.na(result$comp_prot_child_sep_cat[1]))
-})
-
-test_that("add_comp_prot throws error for missing columns", {
-  df_test <- df_dummy
-  df_test <- df_test %>% select(-prot_child_sep_cat)
-  expect_error(add_comp_prot(df_test), class = "error")
 })
 
 test_that("add_comp_prot assigns in need status correctly", {
