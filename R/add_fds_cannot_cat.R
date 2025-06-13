b3af97ad5a4a41e4964010324757edc369289731
sunheemoraes <sunheemoraes@gmail.com>
2025-06-13 11:21:03 +0200

    update issue586 and issue587

--- R/add_fds_cannot_cat.R
+++ R/add_fds_cannot_cat.R
@@ -1,30 +1,22 @@
 #' @title Add Functional Domestic Space Tasks Categories
 #'
 #' @description This function adds categories for functional domestic space tasks based on various input parameters.
-#' It processes cooking, sleeping, storing, personal hygiene, and lighting tasks, creating new columns with standardized categories and a summary of tasks that cannot be performed.
+#' It processes cooking, sleeping, storing, and lighting tasks, creating new columns with standardized categories and a summary of tasks that cannot be performed.
 #'
 #' @param df A data frame containing the input columns
 #' @param fds_cooking Column name for cooking tasks
 #' @param fds_cooking_cannot Value for cannot perform cooking tasks
-#' @param fds_cooking_can_issues Value for can perform cooking tasks but with issues
-#' @param fds_cooking_can_no_issues Value for can perform cooking tasks without issues
+#' @param fds_cooking_can Value for can perform cooking tasks
 #' @param fds_cooking_no_need Value for no need to perform cooking tasks
 #' @param fds_cooking_undefined Vector of undefined responses for cooking tasks
 #' @param fds_sleeping Column name for sleeping tasks
 #' @param fds_sleeping_cannot Value for cannot perform sleeping tasks
-#' @param fds_sleeping_can_issues Value for can perform sleeping tasks but with issues
-#' @param fds_sleeping_can_no_issues Value for can perform sleeping tasks without issues
+#' @param fds_sleeping_can Value for can perform sleeping tasks
 #' @param fds_sleeping_undefined Vector of undefined responses for sleeping tasks
 #' @param fds_storing Column name for storing tasks
 #' @param fds_storing_cannot Value for cannot perform storing tasks
-#' @param fds_storing_can_issues Value for can perform storing tasks but with issues
-#' @param fds_storing_can_no_issues Value for can perform storing tasks without issues
+#' @param fds_storing_can Value for can perform storing tasks
 #' @param fds_storing_undefined Vector of undefined responses for storing tasks
-#' @param fds_personal_hygiene Column name for personal hygiene tasks
-#' @param fds_personal_hygiene_cannot Value for cannot perform personal hygiene tasks
-#' @param fds_personal_hygiene_can_issues Value for can perform personal hygiene tasks but with issues
-#' @param fds_personal_hygiene_can_no_issues Value for can perform personal hygiene tasks without issues
-#' @param fds_personal_hygiene_undefined Vector of undefined responses for personal hygiene tasks
 #' @param lighting_source Column name for lighting source
 #' @param lighting_source_none Value for no lighting source
 #' @param lighting_source_undefined Vector of undefined responses for lighting source
@@ -34,7 +26,6 @@
 #' * snfi_fds_cooking: Standardized categories for cooking tasks
 #' * snfi_fds_sleeping: Standardized categories for sleeping tasks
 #' * snfi_fds_storing: Standardized categories for storing tasks
-#' * snfi_fds_personal_hygiene: Standardized categories for personal hygiene tasks
 #' * energy_lighting_source: Standardized categories for lighting source
 #' * snfi_fds_cannot_n: Number of tasks that cannot be performed
 #' * snfi_fds_cannot_cat: Categorized number of tasks that cannot be performed
@@ -49,14 +40,13 @@ add_fds_cannot_cat <- function(
     fds_cooking_no_need = "no_need",
     fds_cooking_undefined = "pnta",
     fds_sleeping = "snfi_fds_sleeping",
-    fds_sleeping_cannot = "no_cannot",
-    fds_sleeping_can_issues = "yes_issues",
-    fds_sleeping_can_no_issues = "yes_no_issues",
-    fds_sleeping_undefined = c("pnta", "dnk"),
+    fds_sleeping_can = "yes",
+    fds_sleeping_cannot = "no",
+    fds_sleeping_undefined = "pnta",
     fds_storing = "snfi_fds_storing",
     fds_storing_cannot = "no",
-    fds_storing_can = c("yes_issues", "yes_no_issues")
-    fds_storing_undefined = c("pnta", "dnk"),
+    fds_storing_can = c("yes_issues", "yes_no_issues"),
+    fds_storing_undefined = "pnta",
     lighting_source = "energy_lighting_source",
     lighting_source_none = "none",
     lighting_source_undefined = c("pnta", "dnk")
@@ -65,21 +55,20 @@ add_fds_cannot_cat <- function(
   #------ Checks
 
   # Check if all columns are present
-  if_not_in_stop(df, c(fds_cooking, fds_sleeping, fds_storing, fds_personal_hygiene, lighting_source), "df")
+  if_not_in_stop(df, c(fds_cooking, fds_sleeping, fds_storing, lighting_source), "df")
 
   # Check if values are in set
-  are_values_in_set(df, fds_cooking, c(fds_cooking, fds_cooking_yes, fds_cooking_no, fds_cooking_no_need, fds_cooking_undefined))
-  are_values_in_set(df, fds_sleeping, c(fds_sleeping_cannot, fds_sleeping_can_issues, fds_sleeping_can_no_issues, fds_sleeping_undefined))
-  are_values_in_set(df, fds_storing, c(fds_storing_cannot, fds_storing_can_issues, fds_storing_can_no_issues, fds_storing_undefined))
-  are_values_in_set(df, fds_personal_hygiene, c(fds_personal_hygiene_cannot, fds_personal_hygiene_can_issues, fds_personal_hygiene_can_no_issues, fds_personal_hygiene_undefined))
+  are_values_in_set(df, fds_cooking, c(fds_cooking_can, fds_cooking_cannot, fds_cooking_no_need, fds_cooking_undefined))
+  are_values_in_set(df, fds_sleeping, c(fds_sleeping_cannot, fds_sleeping_can, fds_sleeping_undefined))
+
+  are_values_in_set(df, fds_storing, c(fds_storing_cannot, fds_storing_can, fds_storing_undefined))
 
   # Checks if all parameters that are not "undefined" are of length 1
   if (length(fds_cooking_cannot) != 1) rlang::abort("fds_cooking_cannot must be of length 1")
-  if (length(fds_cooking_can) != 1) rlang::abort("fds_cooking_can_issues must be of length 1")
+  if (length(fds_cooking_can) != 1) rlang::abort("fds_cooking_can must be of length 1")
   if (length(fds_cooking_no_need) != 1) rlang::abort("fds_cooking_no_need must be of length 1")
   if (length(fds_sleeping_cannot) != 1) rlang::abort("fds_sleeping_cannot must be of length 1")
-  if (length(fds_sleeping_can_issues) != 1) rlang::abort("fds_sleeping_can_issues must be of length 1")
-  if (length(fds_sleeping_can_no_issues) != 1) rlang::abort("fds_sleeping_can_no_issues must be of length 1")
+  if (length(fds_sleeping_can) != 1) rlang::abort("fds_sleeping_can must be of length 1")
   if (length(fds_storing_cannot) != 1) rlang::abort("fds_storing_cannot must be of length 1")
   if (length(lighting_source_none) != 1) rlang::abort("lighting_source_none must be of length 1")
 
@@ -95,15 +84,14 @@ add_fds_cannot_cat <- function(
       ),
       snfi_fds_sleeping = dplyr::case_when(
         !!rlang::sym(fds_sleeping) == fds_sleeping_cannot ~ "no_cannot",
-        !!rlang::sym(fds_sleeping) == fds_sleeping_can_issues ~ "yes_issues",
-        !!rlang::sym(fds_sleeping) == fds_sleeping_can_no_issues ~ "yes_no_issues",
-        !!rlang::sym(fds_sleeping) %in% fds_sleeping_undefined ~ "undefined",
+        !!rlang::sym(fds_sleeping) == fds_sleeping_can ~ "yes",
+        !!rlang::sym(fds_sleeping) == fds_sleeping_undefined ~ "undefined",
         .default = NA_character_
       ),
       snfi_fds_storing = dplyr::case_when(
         !!rlang::sym(fds_storing) == fds_storing_cannot ~ "no_cannot",
-        !!rlang::sym(fds_storing) == fds_storing_can ~ "yes",
-        !!rlang::sym(fds_storing) %in% fds_storing_undefined ~ "undefined",
+        !!rlang::sym(fds_storing) %in% fds_storing_can ~ "yes",
+        !!rlang::sym(fds_storing) == fds_storing_undefined ~ "undefined",
         .default = NA_character_
       ),
       energy_lighting_source = dplyr::case_when(
@@ -121,7 +109,7 @@ add_fds_cannot_cat <- function(
       c("snfi_fds_cooking", "snfi_fds_sleeping", "snfi_fds_storing"),
       \(x) dplyr::case_when(
         x == "no_cannot" ~ 1,
-        x %in% c("yes", "yes_issues", "yes_no_issues", "no_no_need") ~ 0,
+        x %in% c("yes", "no_no_need") ~ 0,
         .default = NA_real_
       ),
       .names = "{.col}_d"
