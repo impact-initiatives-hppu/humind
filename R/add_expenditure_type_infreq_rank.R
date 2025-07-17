a2c96856820dacc52e7334c029dea4179e1dfb96
Iyed GHEDAMSI <iyed.ghedamsi@impact-initiatives.org>
2025-07-17 13:53:34 +0300

    :necktie: (add_expenditure_type_infreq_rank) add new infrequent expenditure type
    Add new infrequent expenditure type
    
    Closes #604

--- R/add_expenditure_type_infreq_rank.R
+++ R/add_expenditure_type_infreq_rank.R
@@ -8,7 +8,7 @@
 #' data frame indicating the top 3 infrequent expenditure types.
 #'
 #' Prerequisite function:
-#' 
+#'
 #' * add_expenditure_type_zero_infreq.R
 #'
 #' @param df A data frame containing infrequent expenditure data for households.
@@ -16,30 +16,32 @@
 #' @param id_col The name of the column that contains the unique identifier.
 #'
 #' @return A data frame with additional columns:
-#' 
+#'
 #' * cm_infreq_expenditure_top1: The most infrequent expenditure type.
 #' * cm_infreq_expenditure_top2: The second most infrequent expenditure type.
 #' * cm_infreq_expenditure_top3: The third most infrequent expenditure type.
 #'
 #' @export
 add_expenditure_type_infreq_rank <- function(
-    df,
-    expenditure_infreq_types = c("cm_expenditure_infrequent_shelter",
-                                 "cm_expenditure_infrequent_nfi",
-                                 "cm_expenditure_infrequent_health",
-                                 "cm_expenditure_infrequent_education",
-                                 "cm_expenditure_infrequent_debt",
-                                 "cm_expenditure_infrequent_other"),
-    id_col = "uuid"){
-
+  df,
+  expenditure_infreq_types = c(
+    "cm_expenditure_infrequent_shelter",
+    "cm_expenditure_infrequent_nfi",
+    "cm_expenditure_infrequent_health",
+    "cm_expenditure_infrequent_education",
+    "cm_expenditure_infrequent_debt",
+    "cm_expenditure_infrequent_clothing",
+    "cm_expenditure_infrequent_other"
+  ),
+  id_col = "uuid"
+) {
   #------ Checks
 
   # Check if the columns exist
   if_not_in_stop(df, expenditure_infreq_types, "df")
 
   # Check that all infrequent expenditure types are here
-  if (length(expenditure_infreq_types) < 6) {
-
+  if (length(expenditure_infreq_types) < 7) {
     rlang::abort("Some of the infrequent expenditure types are null.")
   }
 
@@ -54,10 +56,8 @@ add_expenditure_type_infreq_rank <- function(
     new_colname_top1 = "cm_infreq_expenditure_top1",
     new_colname_top2 = "cm_infreq_expenditure_top2",
     new_colname_top3 = "cm_infreq_expenditure_top3",
-    id_col = id_col)
+    id_col = id_col
+  )
 
   return(df)
-
-
-
 }
