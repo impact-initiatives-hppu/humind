f98916c8d1fc11178ad71fd46f18bbcfbe49b2dd
Iyed GHEDAMSI <iyed.ghedamsi@impact-initiatives.org>
2025-07-22 12:11:43 +0300

    Improve tests Dummy data generator improved to correctly match generation constraints.

--- tests/testthat/test-add_prot_needs_movement.R
+++ tests/testthat/test-add_prot_needs_movement.R
@@ -37,7 +37,9 @@ gen_dummy_sm <- function() {
     sim_df,
     `prot_needs_3_movement/dnk` + `prot_needs_3_movement/pnta` <= 1,
     tot < 14,
-    tot > 0
+    tot > 0,
+    !(`prot_needs_3_movement/pnta` == 1 & tot > 1),
+    !(`prot_needs_3_movement/dnk` == 1 & tot > 1)
   )
   return(sim_df)
 }
@@ -50,15 +52,14 @@ test_that("column names checked correctly", {
     add_prot_needs_movement(dummy_df)
   )
 
-  res <- add_prot_needs_movement(dummy_df)
-  summary(res)
-
+  # Should error if the column names are not as expected
   modified_dummy_sm <- gen_dummy_sm() |>
     dplyr::rename_with(.fn = \(x) stringr::str_replace(x, "/", "_"))
   expect_error(
     add_prot_needs_movement(modified_dummy_sm)
   )
 
+  # If the user has a different name for an answer option, by default it should error
   non_default_dummy_sm <- dummy_df |>
     dplyr::rename(`prot_needs_3_movement/dnk2` = `prot_needs_3_movement/dnk`)
 
@@ -66,6 +67,7 @@ test_that("column names checked correctly", {
     add_prot_needs_movement(non_default_dummy_sm)
   )
 
+  # If the user has a different name for an answer option and provides the changed name, it should not error
   expect_no_error(
     add_prot_needs_movement(non_default_dummy_sm, dnk = "dnk2")
   )
@@ -85,12 +87,14 @@ test_that("weighting done correctly", {
   expect_true(all(is.na(res$`prot_needs_3_movement/dnk`)))
   expect_true(all(is.na(res$`prot_needs_3_movement/pnta`)))
   expect_true(all(is.na(res$`prot_needs_3_movement/other_safety_measures`)))
-
-  # No column should have a value higher than the maximum weight.
-  # TODO: Maximum weight is hardcoded for now, DRY later
 })
 
 test_that("composite value calculated correctly", {
   res <- add_prot_needs_movement(dummy_df)
   expect_true("comp_prot_score_prot_needs_3" %in% names(res))
+
+  # Final composite score should be between 0 and 4
+  max_weight <- 4
+  expect_true(all(res$comp_prot_score_needs_1 <= max_weight))
+  expect_true(all(res$comp_prot_score_needs_1 > 0))
 })
