30ef8652b8651aa2f4968b230ff7358a6761532c
Iyed GHEDAMSI <iyed.ghedamsi@impact-initiatives.org>
2025-07-28 13:01:33 +0300

    [TESTS] Resolve warning about implicit handling of NA vector with `max` `num_cat` did not explicitly provide a default value for when `max` is called with an all NA vector which raises a warning. By default `max` will return `-Inf` with a warning. It has been made explicit.
    Loading `testthat` and `dplyr` within the test file has been removed.

--- R/num_cat.R
+++ R/num_cat.R
@@ -1,7 +1,7 @@
 #' @title Add Categories for a Numeric Variable
-#' 
+#'
 #' @description This function categorizes a numeric column in a dataframe based on specified breaks and labels. It can handle undefined values and provides options for customizing the categorization process.
-#' 
+#'
 #' @param df A dataframe containing the numeric column to be categorized.
 #' @param num_col The column name to recategorize.
 #' @param breaks A vector of cut points for categorization.
@@ -11,14 +11,23 @@
 #' @param new_colname The name of the new column. If NULL, it's automatically generated.
 #' @param plus_last Logical, whether to add a "+" to the last category.
 #' @param above_last Logical, whether to add a category for values above the last break.
-#' 
+#'
 #' @return A dataframe with an additional column:
-#' 
+#'
 #' * new_colname: A new column containing the categorized values of the original numeric column.
 #'
 #' @export
-num_cat <- function(df, num_col, breaks, labels = NULL, int_undefined = c(-999, 999),
-                    char_undefined = "Unknown", new_colname = NULL, plus_last = FALSE, above_last = FALSE) {
+num_cat <- function(
+  df,
+  num_col,
+  breaks,
+  labels = NULL,
+  int_undefined = c(-999, 999),
+  char_undefined = "Unknown",
+  new_colname = NULL,
+  plus_last = FALSE,
+  above_last = FALSE
+) {
   #------ Checks
 
   # Check if num_col is in df
@@ -28,16 +37,24 @@ num_cat <- function(df, num_col, breaks, labels = NULL, int_undefined = c(-999, 
   are_cols_numeric(df, num_col)
 
   # Check if int_dontknow is numeric
-  if (!all(sapply(int_undefined, is.numeric))) rlang::abort("int_dontknow must only contain numeric values")
+  if (!all(sapply(int_undefined, is.numeric))) {
+    rlang::abort("int_dontknow must only contain numeric values")
+  }
 
   # Check if char_dontknow is of length 1
-  if (length(char_undefined) != 1) rlang::abort("char_dontknow must be of length 1")
+  if (length(char_undefined) != 1) {
+    rlang::abort("char_dontknow must be of length 1")
+  }
 
   # Check if char_dontknow is character
-  if (!is.character(char_undefined)) rlang::abort("char_dontknow must be a character")
+  if (!is.character(char_undefined)) {
+    rlang::abort("char_dontknow must be a character")
+  }
 
   # Check if breaks have at least two values
-  if (length(breaks) < 2) rlang::abort("breaks must have at least two values")
+  if (length(breaks) < 2) {
+    rlang::abort("breaks must have at least two values")
+  }
 
   # Check that above_last is logical and not NA
   if (!is.logical(above_last) || is.na(above_last)) {
@@ -46,17 +63,19 @@ num_cat <- function(df, num_col, breaks, labels = NULL, int_undefined = c(-999, 
 
   # Check if labels is of length of breaks if above_last is TRUE
   if (!is.null(labels) && length(labels) != length(breaks) && above_last) {
-    rlang::warn("labels must be of length of breaks. Reverting to labels = NULL")
+    rlang::warn(
+      "labels must be of length of breaks. Reverting to labels = NULL"
+    )
     labels <- NULL
   }
 
   # Check if labels is of length of breaks - 1 if above_last is FALSE
   if (!is.null(labels) && length(labels) != length(breaks) - 1 && !above_last) {
-    rlang::warn("labels must be of length of breaks - 1. Reverting to labels = NULL")
+    rlang::warn(
+      "labels must be of length of breaks - 1. Reverting to labels = NULL"
+    )
     labels <- NULL
   }
-
-
 
   # Check that plus_last is logical and not NA
   if (!is.logical(plus_last) || is.na(plus_last)) {
@@ -64,27 +83,34 @@ num_cat <- function(df, num_col, breaks, labels = NULL, int_undefined = c(-999, 
   }
 
   # If new_colname is not provided, create one
-  if (is.null(new_colname)) new_colname <- paste0(num_col, "_cat")
+  if (is.null(new_colname)) {
+    new_colname <- paste0(num_col, "_cat")
+  }
 
   #----- Create labels if NULL
 
   vals <- df[[num_col]]
   vals <- vals[!(vals %in% int_undefined)]
-  max_val <- max(vals, na.rm = TRUE)
+  max_val <- max(vals, na.rm = TRUE, default = -Inf)
 
   # Total number of breaks
-  if (above_last) breaks <- c(breaks, Inf)
+  if (above_last) {
+    breaks <- c(breaks, Inf)
+  }
 
   # If labels are not provided, generate them
   if (is.null(labels)) {
-
     labels <- sapply(1:(length(breaks) - 1), function(i) {
       lower <- breaks[i]
       upper <- breaks[i + 1]
       if (i == length(breaks) - 1 && plus_last) {
         return(paste0(lower, "+"))
       } else if (i == length(breaks) - 1 && !plus_last) {
-        if (lower == max_val) return(paste0(lower, "+")) else return(paste0(lower, "-", max_val))
+        if (lower == max_val) {
+          return(paste0(lower, "+"))
+        } else {
+          return(paste0(lower, "-", max_val))
+        }
       } else {
         return(paste0(lower, "-", upper - 1))
       }
@@ -92,17 +118,21 @@ num_cat <- function(df, num_col, breaks, labels = NULL, int_undefined = c(-999, 
   }
 
   # Create categories
-  df <- dplyr::mutate(df, "{new_colname}" := dplyr::case_when(
-    # Replace value in int_dontkow by char_dontknow
-    !!rlang::sym(num_col) %in% int_undefined ~ char_undefined,
-    # Replace values below min(breaks) by NA
-    !!rlang::sym(num_col) < min(breaks) ~ NA_character_,
-    # Create categories using function cut and adjusted breaks
-    .default = cut(!!rlang::sym(num_col),
-                   breaks = breaks,
-                   labels = labels,
-                   include.lowest = TRUE,
-                   right = FALSE)
+  df <- dplyr::mutate(
+    df,
+    "{new_colname}" := dplyr::case_when(
+      # Replace value in int_dontkow by char_dontknow
+      !!rlang::sym(num_col) %in% int_undefined ~ char_undefined,
+      # Replace values below min(breaks) by NA
+      !!rlang::sym(num_col) < min(breaks) ~ NA_character_,
+      # Create categories using function cut and adjusted breaks
+      .default = cut(
+        !!rlang::sym(num_col),
+        breaks = breaks,
+        labels = labels,
+        include.lowest = TRUE,
+        right = FALSE
+      )
     )
   )
 
