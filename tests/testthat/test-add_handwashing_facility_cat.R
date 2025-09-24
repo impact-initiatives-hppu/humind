73d4cd685934b35bf9e160cbf89e70de649ddae2
Iyed GHEDAMSI <iyed.ghedamsi@impact-initiatives.org>
2025-09-24 14:27:23 +0300

    fix(add_handwashing_facility_cat): support vector 'no' codes, prevent misclassification, update docs * Switched equality checks to %in% for water/soap 'no' codes. * Updated parameter docs to allow multiple response codes and note   non-qualifying soap types (ash, mud, sand, etc.). * Adjusted defaults to vectors for clarity. * Added regression test: scalar vs vector 'no' inputs now produce   identical classifications (no silent recycling).
    Closes #643

--- tests/testthat/test-add_handwashing_facility_cat.R
+++ tests/testthat/test-add_handwashing_facility_cat.R
@@ -1,76 +1,76 @@
-test_that("add_handwashing_facility_cat categorizes correctly", {
-  test_df <- dplyr::tibble(
-    survey_modality = c(
-      "in_person",
-      "in_person",
-      "in_person",
-      "in_person",
-      "remote",
-      "remote",
-      "in_person",
-      "in_person"
-    ),
-    wash_handwashing_facility = c(
-      "available_fixed_in_dwelling",
-      "available_fixed_in_plot",
-      "available_fixed_in_plot",
-      "none",
-      "available_fixed_or_mobile",
-      "none",
-      "no_permission",
-      "other"
-    ),
-    wash_handwashing_facility_observed_water = c(
-      "water_available",
-      "water_not_available",
-      "water_available",
-      "water_available",
-      NA,
-      NA,
-      NA,
-      "water_available"
-    ),
-    wash_soap_observed = c(
-      "yes_soap_shown",
-      "yes_soap_shown",
-      "no",
-      "yes_soap_shown",
-      NA,
-      NA,
-      NA,
-      "yes_soap_shown"
-    ),
-    wash_handwashing_facility_reported = c(
-      NA,
-      NA,
-      NA,
-      NA,
-      "fixed_dwelling",
-      "none",
-      "mobile",
-      "other"
-    ),
-    wash_handwashing_facility_water_reported = c(
-      NA,
-      NA,
-      NA,
-      NA,
-      "yes",
-      "no",
-      "yes",
-      "dnk"
-    ),
-    wash_soap_reported = c(
-      NA,
-      NA,
-      NA,
-      NA,
-      "yes",
-      "no",
-      "yes",
-      "dnk"
-    )
+test_df <- dplyr::tibble(
+  survey_modality = c(
+    "in_person",
+    "in_person",
+    "in_person",
+    "in_person",
+    "remote",
+    "remote",
+    "in_person",
+    "in_person"
+  ),
+  wash_handwashing_facility = c(
+    "available_fixed_in_dwelling",
+    "available_fixed_in_plot",
+    "available_fixed_in_plot",
+    "none",
+    "available_fixed_or_mobile",
+    "none",
+    "no_permission",
+    "other"
+  ),
+  wash_handwashing_facility_observed_water = c(
+    "water_available",
+    "water_not_available",
+    "water_available",
+    "water_available",
+    NA,
+    NA,
+    NA,
+    "water_available"
+  ),
+  wash_soap_observed = c(
+    "yes_soap_shown",
+    "yes_soap_shown",
+    "no",
+    "yes_soap_shown",
+    NA,
+    NA,
+    NA,
+    "yes_soap_shown"
+  ),
+  wash_handwashing_facility_reported = c(
+    NA,
+    NA,
+    NA,
+    NA,
+    "fixed_dwelling",
+    "none",
+    "mobile",
+    "other"
+  ),
+  wash_handwashing_facility_water_reported = c(
+    NA,
+    NA,
+    NA,
+    NA,
+    "yes",
+    "no",
+    "yes",
+    "dnk"
+  ),
+  wash_soap_reported = c(
+    NA,
+    NA,
+    NA,
+    NA,
+    "yes",
+    "no",
+    "yes",
+    "dnk"
   )
+)
+test_that("add_handwashing_facility_cat categorizes correctly", {
   expected <- c(
     "basic",
     "limited",
@@ -83,4 +83,90 @@ test_that("add_handwashing_facility_cat categorizes correctly", {
   )
   result_df <- add_handwashing_facility_cat(test_df)
   expect_equal(result_df$wash_handwashing_facility_jmp_cat, expected)
+})
+
+
+survey_modality <- c(
+  "in_person",
+  "remote"
+)
+
+wash_handwashing_facility <- c(
+  "available_fixed_in_dwelling",
+  "available_fixed_in_plot",
+  "available_fixed_or_mobile",
+  "none",
+  "no_permission",
+  "other"
+)
+wash_handwashing_facility_observed_water <- c(
+  "water_available",
+  "water_not_available",
+  "water_available",
+  NA
+)
+
+wash_soap_observed <- c(
+  "yes_soap_shown",
+  "no",
+  NA
+)
+
+
+wash_handwashing_facility_reported <- c(
+  "fixed_dwelling",
+  "none",
+  "mobile",
+  "other",
+  NA
+)
+
+wash_handwashing_facility_water_reported <- c(
+  "yes",
+  "no",
+  "yes",
+  "dnk",
+  NA
+)
+
+wash_soap_reported <- c(
+  "yes",
+  "no",
+  "yes",
+  "dnk",
+  NA
+)
+
+exhaustive_df <- tidyr::expand_grid(
+  survey_modality,
+  wash_handwashing_facility,
+  wash_handwashing_facility_observed_water,
+  wash_soap_observed,
+  wash_handwashing_facility_reported,
+  wash_handwashing_facility_water_reported,
+  wash_soap_reported
+)
+
+test_that("Response codes with multiple answer options do not affect the result when provided", {
+  result_scalar <- add_handwashing_facility_cat(
+    exhaustive_df,
+    facility_observed_water_no = "water_not_available",
+    facility_observed_soap_no = "no",
+    facility_reported_no = "none",
+    facility_reported_water_no = "no",
+    facility_reported_soap_no = "no"
+  )
+
+  # Did not include 'facility_no' because the code will raise en error
+  # and it already checks that it is exactly one value
+  result_vector <- add_handwashing_facility_cat(
+    exhaustive_df,
+    facility_observed_water_no = c("water_not_available", "indifferent"),
+    facility_observed_soap_no = c("no", "indifferent"),
+    facility_reported_no = c("none", "indifferent"),
+    facility_reported_water_no = c("no", "indifferent"),
+    facility_reported_soap_no = c("no", "indifferent")
+  )
+
+  expect_equal(result_scalar, result_vector)
 })
